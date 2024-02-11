8b55f0d309c189b408452e23fead4e64d5c94d26
MURIITHIABEL <abelmuriithi04@gmail.com>
2024-02-11 22:44:42 +0300

    second

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-     <p id="moja">abel</p>
+     <h5 id="moja">abel</h5>
      <p id="three">Muriithi</p>
      <p id="three">Mugambi</p>
     </div>
