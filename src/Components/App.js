f8cdcbc4e3325c7f9404d488ddd4df84fd30d8a6
MURIITHIABEL <abelmuriithi04@gmail.com>
2024-02-11 22:49:14 +0300

    third

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-     <h5 id="moja">abel</h5>
+     <h5 id="moja">abel-updated in abel branch</h5>
      <p id="three">Muriithi</p>
      <p id="three">Mugambi</p>
     </div>
