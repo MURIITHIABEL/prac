de6f847513b3d01e02ea1971536f52b6dff079bd
MURIITHIABEL <abelmuriithi04@gmail.com>
2024-02-11 23:11:40 +0300

    main

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
-
 function App() {
   return (
     <div className="App">
-     <h5 id="moja">abellll - updated on abelo branch</h5>
-     <p id="three">Muriithi</p>
-   
+      <h5 id="moja">abel - Updated in main branch</h5>
+      <p id="three">Muriithi - Updated in main branch</p>
+      <p id="three">Mugambi</p>
     </div>
   );
 }
 
 export default App;
+
